48c77828915a7fed99e3e3a498adfe32933398cd
tang <hasee@DESKTOP-BVJGSVE>
2018-01-24 11:07:02 +0800

    这是我今天提交的

---
+++ maven_java/src/main/java/com/jdbc/Jdbc.java
@@ -0,0 +1,29 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+public class Jdbc {
+	public static void main(String[] args) {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql:///getor", "root", "19980421");
+			System.out.println(conn==null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void getconn(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql:///getor", "root", "19980421");
+			System.out.println(conn==null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
