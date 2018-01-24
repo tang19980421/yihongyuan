80980eea2d3e7a63e330266e0000c3fc76e6daa8
lenovo <lenovo@DESKTOP-94P1V2L>
2018-01-24 12:15:35 +0800

    aAS

--- maven_java/src/main/java/com/jdbc/Jdbc.java
+++ maven_java/src/main/java/com/jdbc/Jdbc.java
@@ -19,6 +19,7 @@ public class Jdbc {
 	
 	public static void getconn(){
 		try {
+			System.out.println("123456");
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = DriverManager.getConnection("jdbc:mysql:///getor", "root", "19980421");
 			System.out.println(conn==null);
